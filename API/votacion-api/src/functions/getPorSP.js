985bd99c793f5ad7725d129a7c01927cea962733
seriousname-90 <144873073+seriousname-90@users.noreply.github.com>
2025-06-10 14:11:50 -0600

    Reorganización de responsabilidades-API

--- API/votacion-api/src/functions/getPorSP.js
+++ API/votacion-api/src/functions/getPorSP.js
@@ -1,28 +1,17 @@
-const { getUserFromToken } = require('../auth');
-const { sql, config } = require('../db/config');
+const { getProvidersBySP } = require('../services/providerService');
 
-module.exports.handler = async (event) => {
+module.exports.handler = async () => {
   try {
-    console.log(event);
-    const tokenPayload = getUserFromToken(event);
-    console.log(tokenPayload);
-
-    await sql.connect(config);
-    const result = await sql.query('EXEC sp_get_api_providers');
-
+    const result = await getProvidersBySP();
     return {
       statusCode: 200,
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(result.recordset)
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(result)
     };
   } catch (err) {
-    console.error(`Error ejecutando SP: ${err.message}`);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: `Error ejecutando SP: ${err.message}` })
+      body: JSON.stringify({ error: 'Error interno', detalles: err.message })
     };
   }
 };
-
