6ba96a75153d128bc0a367516c074681bc8e9264
JoseMonge17 <josejulianmonge@gmail.com>
2025-06-16 14:12:28 -0600

    Endpoint listar votaciones

--- API/votacion-api/src/services/voteService.js
+++ API/votacion-api/src/services/voteService.js
@@ -1,6 +1,5 @@
 const { findById, getDemographicData } = require('../data/authUserData');
-const { getSessionById, getVotingRulesForSession, hasUserVoted, registerEncryptedVote, createEligibility, updateDemographicStat, updateCommitment, backupVote } = require('../data/voteData');
-const { getUserFromToken } = require('../auth');
+const { getSessionById, getVotingRulesForSession, hasUserVoted, registerEncryptedVote, createEligibility, updateDemographicStat, updateCommitment, backupVote, getLastFiveVotes, getQuestionsAndOptions } = require('../data/voteData');
 const { sequelize } = require('../db/sequelize');
 const { verifyMfaCode } = require('../data/MfaVerification');
 const { saveLivenessData } = require('../data/livenessData');
@@ -164,4 +163,120 @@ async function vote(data, body)
     }
 }
 
-module.exports = { vote };
+async function listVotes(data, body) 
+{
+    const user = data.user
+
+    // Validar autenticación multifactor (MFA) y comprobación de vida
+    const { authenticated, error } = await verifyMfaCode(body.methodid, body.codeMFA);
+
+    if (!authenticated) 
+    {
+        throw new Error(error);
+    }
+
+    //const result = await saveLivenessData(body.livenessCheck, body.biometricMedia, user.userid);
+
+    //if(!result.success) throw new Error(result.error);
+
+    if(!body.livenessCheck.result) throw new Error("Identidad no confirmada")
+
+    // Confirmar existencia activa del ciudadano en el sistema
+    if (!user) throw new Error('Usuario no encontrado');
+
+    //      Validar estado
+    if (user.status.name !== "Active") {
+        throw new Error(`Usuario en estado '${user.status.name}'`);
+    }
+
+    // Consultar en la base las cinco últimas propuestas en las que ha participado mediante voto
+
+    const ballots = await getLastFiveVotes(user.userid);
+
+    const votosSecretos = [];
+
+    // Obtener la llave criptográfica del usuario
+    const userKey = Buffer.from(data.userkey.publicKey.data);
+
+    // Extraer los votos asociados, descifrarlos y mostrar: propuesta, fecha y decisión (resumen, no detalle)
+    for (const ballot of ballots) 
+    {
+        let voto = null
+
+        if (!ballot.userid) voto = decryptVote(ballot.encryptedVote, userKey);
+        else voto = decodeJson(ballot.encryptedVote);
+
+        if (voto) 
+        {
+            votosSecretos.push({
+                fecha: new Date(ballot.voteDate).toLocaleString('es-CR', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hour12: true,
+                    timeZone: 'America/Costa_Rica'
+                }),
+                decision: await formatearVotoEstructurado(voto)
+            });
+        }
+    }
+
+    return votosSecretos;
+}
+
+function decodeJson(buffer) 
+{
+    try {
+        const jsonStr = Buffer.from(buffer).toString('utf-8');
+        return JSON.parse(jsonStr);
+    } catch (err) {
+        console.error('Error al decodificar voto:', err);
+        return null;
+    }
+}
+
+function decryptVote(encryptedBuffer, keyBuffer) {
+    try {
+        const aesKey = crypto.createHash('sha256').update(keyBuffer).digest();
+
+        const decipher = crypto.createDecipheriv('aes-256-ecb', aesKey, null);
+        decipher.setAutoPadding(true);
+
+        const encryptedBase64 = Buffer.from(encryptedBuffer).toString('base64');
+
+        let decrypted = decipher.update(encryptedBase64, 'base64', 'utf8');
+        decrypted += decipher.final('utf8');
+
+        return JSON.parse(decrypted);
+    } catch (err) {
+        console.error('❌ Error al desencriptar el voto secreto:', err.message);
+        return null;
+    }
+}
+
+async function formatearVotoEstructurado(voto) 
+{
+    const questionIds = voto.map(v => v.questionid);
+
+    const { questions, options } = await getQuestionsAndOptions(questionIds);
+
+    return voto.map(respuesta => 
+    {
+        const pregunta = questions.find(q => q.questionid === respuesta.questionid);
+        const respuestas = respuesta.optionsid.map(id => 
+        {
+            const op = options.find(o => o.optionid === id);
+            return op.description;
+        });
+
+        return {
+            Pregunta: pregunta ? pregunta.description : '(pregunta desconocida)',
+            Respuestas: respuestas
+        };
+    });
+}
+
+module.exports = { vote, listVotes };
